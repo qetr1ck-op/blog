86e3a152d5f9f38a439e20738894a44d812900f1
qetr1ck-oop <orestprustyko@gmail.com>
2017-03-04 22:44:53 +0200

    update config | add page

---
+++ js/ga-seach.js
@@ -0,0 +1,9 @@
+(function() {
+var cx = '016073824135735695849:_l_1tqmypje';
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gcse, s);
+})();
